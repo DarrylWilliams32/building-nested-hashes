4f1fc2838761e5e469f94a242c6e2cfd7e7673fc
darryl_williams <darrylwilliams6@gmail.com>
2021-02-14 00:59:43 -0500

    commit tree a2420dc25e345c6527c1f630e8c7f144ed5e4df6

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,9 @@
+#create data list, put hashes of montague and capulet inside main hash
+#set the keys of the hashes as symbols, remember: symbols are immutable. Since we won't be changing
+#these keys, it makes sense to make them symbols.
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = {
+      :montague => {}, 
+      :capulet => {}
+   }
 end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,10 +2,19 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {}, 
+      :matriarch => {},
+      :hero => {}, 
+      :hero_friends => [] #hero friends is an array to store hashes of a list of the hero friends
       }, 
     :capulet => {
       #your key/value pairs here!
-      }}
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {}, 
+      :heroine_friends => [] 
+      }
+    }
 
   
 end
